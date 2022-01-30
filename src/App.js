a0bc13c022988f7dceaee86b6be013cb7c0e5d1a
Adhi Kurniawan <adhikurniawan2108@gmail.com>
2022-01-30 12:30:10 +0700

    components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import "./App.css";
+import "./index.css";
 
-import logo from "./logo.svg";
+import GitData from "./components/GitData";
+import SearchBox from "./components/SearchBox";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div style={{ padding: "1rem" }}>
+            GitHub Repos Finder
+            <SearchBox />
+            <GitData />
         </div>
     );
 }
---
+++ src/components/SearchBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBox = () => {
+    return (
+        <div>
+            <input type="text" placeholder="Search..." />
+        </div>
+    );
+};
+
+export default SearchBox;
