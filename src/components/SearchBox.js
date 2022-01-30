6c1f5f3d794aefaad148b4f0112363c554afb5e2
Adhi Kurniawan <adhikurniawan2108@gmail.com>
2022-01-30 13:03:03 +0700

    search axios components

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
+import axios from "axios";
 
 const SearchBox = () => {
+    const [input, setInput] = useState("");
+    const [repo, setRepo] = useState([]);
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+        // console.log(e.target.value);
+    };
+
+    const handleClick = async () => {
+        console.log(input);
+        try {
+            const result = await axios(`https://api.github.com/users/${input}`);
+            setRepo(result);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    console.log(repo);
     return (
         <div>
-            <input type="text" placeholder="Search..." />
+            <input
+                type="text"
+                placeholder="Search..."
+                value={input}
+                onChange={handleChange}
+            />
+            <button onClick={handleClick}>Search</button>
         </div>
     );
 };
